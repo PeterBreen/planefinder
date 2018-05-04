1c56fc2bdeaa6fca52144c83c60be9549e3e8d21
Peter Breen <pbreen@gmail.com>
2018-05-04 10:46:59 -0700

    probably overcomplicated mapfilter to try and return one result based on URL (props.url.query.title)

--- pages/plane.js
+++ pages/plane.js
@@ -1,8 +1,24 @@
 import Page from '../layouts/main'
 
-export default (props) => (
+const Plane = (props) => (
     <Page>
        <h1>{props.url.query.title}</h1>
-       <p>This is the plane detail page content.</p>
+
+      {props.planes.filter(function (characteristics) {
+         return characteristics.url === "Airbus-A319"
+       }).map(({characteristics}) => (
+         <p key={characteristics.model}>
+          The {characteristics.manufacturer} {characteristics.model} is a {characteristics.body}body aircraft with {characteristics.engines.toString()} engines, a {characteristics.nosecone} nose, and {characteristics.overwingexits.toString()} overwing exits.
+         </p>
+       ))}
+
     </Page>
 )
+
+
+Plane.getInitialProps = async function() {
+  const data = await import('../data/planes.json')
+  return { planes: data };
+}
+
+export default Plane
