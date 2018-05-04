678870fc4ff82fdd4819e5bbe303a1d089e81de1
Peter Breen <pbreen@gmail.com>
2018-05-04 11:02:41 -0700

    add ternary operator to render for dorsalbump - example for other true/false factors

--- pages/plane.js
+++ pages/plane.js
@@ -8,7 +8,7 @@ const Plane = (props) => (
          return props.url.query.title === characteristics.url
        }).map(({characteristics}) => (
          <p key={characteristics.model}>
-          The {characteristics.manufacturer} {characteristics.model} is a {characteristics.body}body aircraft with {characteristics.engines.toString()} {characteristics.engineloc} engines, a {characteristics.nose} nose, and {characteristics.overwingexits.toString()} overwing exits.
+          The {characteristics.manufacturer} {characteristics.model} is a {characteristics.body}body aircraft with {characteristics.engines.toString()} {characteristics.engineloc} engines, a {characteristics.nose} nose, {characteristics.dorsalbump ? 'a ' : 'no'} dorsal bump ahead of the vertical stabilizer, and {characteristics.overwingexits.toString()} overwing exits.
          </p>
        ))}
 
