8653b18afb6417b94c67a0d7ca32002f279de550
Peter Breen <pbreen@gmail.com>
2018-05-04 22:56:22 -0700

    retitle page to read better and add h1 style

--- pages/plane.js
+++ pages/plane.js
@@ -2,17 +2,24 @@ import Page from '../layouts/main'
 
 const Plane = (props) => (
     <Page>
-       <h1>{props.url.query.title}</h1>
-
       {props.planes.filter(function (characteristics) {
          return props.url.query.title === characteristics.url
        }).map(({characteristics}) => (
-         <p key={characteristics.model}>
-          The {characteristics.manufacturer} {characteristics.model} is a {characteristics.body}body aircraft with {characteristics.engines.toString()} {characteristics.engineloc} engines, a {characteristics.nose} nose, {characteristics.dorsalbump ? 'a ' : 'no'} dorsal bump ahead of the vertical stabilizer, and {characteristics.overwingexits.toString()} overwing exits.
-         </p>
+        <div>
+          <h1>{characteristics.manufacturer} {characteristics.model}</h1>
+           <p key={characteristics.model}>
+            The {characteristics.manufacturer} {characteristics.model} is a {characteristics.body}body aircraft with {characteristics.engines.toString()} {characteristics.engineloc} engines, a {characteristics.nose} nose, {characteristics.dorsalbump ? 'a ' : 'no'} dorsal bump ahead of the vertical stabilizer, and {characteristics.overwingexits.toString()} overwing exits.
+           </p>
+        </div>
        ))}
-
+       <style jsx>{`
+         h1 {
+           font-size: 2.5rem;
+           margin-bottom: 1rem;
+         }
+       `}</style>
     </Page>
+
 )
 
 
