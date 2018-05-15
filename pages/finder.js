a7bf3a70b2e96221d6ea9015454ced16048ba05b
Peter Breen <pbreen@gmail.com>
2018-05-15 12:57:44 -0700

    setup skeleton for planefinder

---
+++ pages/finder.js
@@ -0,0 +1,23 @@
+import Page from '../layouts/main'
+
+const Finder = (props) => (
+    <Page>
+      <div>
+      <h1>Plane Finder</h1>
+      <p>Answer the following questions to determine what your plane is.</p>
+
+    
+
+      </div>
+       <style jsx>{`
+         h1 {
+           font-size: 2.5rem;
+           margin-bottom: 1rem;
+         }
+       `}</style>
+    </Page>
+
+)
+
+
+export default Finder
