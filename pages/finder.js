43a4f088188dae85979a69e601df874c14d1e76f
Peter Breen <pbreen@gmail.com>
2018-05-16 11:09:01 -0700

    handleSubmit now sets state to correct state key/val based on event.target.id

--- pages/finder.js
+++ pages/finder.js
@@ -4,21 +4,20 @@ import Page from '../layouts/main'
 class Finder extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {characteristics: {
+    this.state = {
         nose: '',
         overwingexits: '',
-      }};
-
+      };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({[event.target.id]: event.target.value});
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    console.log('handleSubmit() just happened to event: ', event)
     event.preventDefault();
   }
 
@@ -29,16 +28,18 @@ class Finder extends React.Component {
         <h1>Plane Finder</h1>
         <p>Answer the following questions to determine what your plane is.</p>
         <h3>Is the nosecone pointy or rounded?</h3>
+
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.nose} id="nose" onChange={(e) => this.handleChange(e)} />
+              <input type="submit" value="Submit" />
+            </form>
+
         <h3>How many overwing exits are there per side?</h3>
 
         <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
+            <input type="text" value={this.state.overwingexits} id="overwingexits" onChange={(e) => this.handleChange(e)} />
           <input type="submit" value="Submit" />
         </form>
-
 
         </div>
       <style jsx>{`
